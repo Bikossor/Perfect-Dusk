f160385f6ad15fd99291a4d6743023e0044b9dc9
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2025-02-20 19:21:00 +0100

    docs: add ColorPalette
    #1

--- website/src/config.ts
+++ website/src/config.ts
@@ -22,3 +22,43 @@ export const CODE_EDITORS: ReadonlyArray<CodeEditor> = [
     addition: "Coming soon",
   },
 ];
+
+type Color = {
+  name: string;
+  hex: string;
+};
+
+export const COLOR_PALETTE: ReadonlyArray<Color> = [
+  {
+    name: "Active",
+    hex: "#e2004f",
+  },
+  {
+    name: "Border",
+    hex: "#444",
+  },
+  {
+    name: "Comments",
+    hex: "#777",
+  },
+  {
+    name: "Variables",
+    hex: "#FFB580",
+  },
+  {
+    name: "Invalid",
+    hex: "#FF5370",
+  },
+  {
+    name: "Operators",
+    hex: "#B580FF",
+  },
+  {
+    name: "Tags",
+    hex: "#f07178",
+  },
+  {
+    name: "Functions",
+    hex: "#AAFF80",
+  },
+];
