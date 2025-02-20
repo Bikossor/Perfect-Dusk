2b3e3b7d5390b69bbf258fff25040fb43367d070
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2025-02-20 18:36:21 +0100

    docs: add Footer component
    #1

--- website/src/config.ts
+++ website/src/config.ts
@@ -1,4 +1,6 @@
 export const APP_NAME = "Perfect Dusk";
 export const APP_DESCRIPTION =
   "Beautiful and accessible dark theme for your favorite code editor";
+export const AUTHOR_NAME = "Bikossor";
+export const AUTHOR_URL = "https://lichten.me";
 
